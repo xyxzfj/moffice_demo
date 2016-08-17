59fc7992607b7d6960b2eb8a9924e9d8b2748ef3
Franky Zhang <xyxzfj@gmail.com>
2016-08-17 21:10:55 +0800

    refactored cn.wps.moffice.demo.fileManager.ListFileActivity#openFile

--- app/src/main/java/cn/wps/moffice/demo/fileManager/ListFileActivity.java
+++ app/src/main/java/cn/wps/moffice/demo/fileManager/ListFileActivity.java
@@ -39,16 +39,16 @@ import cn.wps.moffice.demo.util.Define;
 import cn.wps.moffice.demo.util.MyGestureListener;
 import cn.wps.moffice.demo.util.SettingPreference;
 
-public class ListFileActivity extends ListActivity 
+public class ListFileActivity extends ListActivity
 {
-	
+
 	private static final String JSON_DATA =
 			"[" +
 			"{ \"name\" : \"cn.wps.moffice.client.OfficeServiceClient\"," +
 			" \"type\" : \"Package-ID\",\"id\" : \"cn.wps.moffice.client\", " +
 			"\"Security-Level\" : \"Full-access\", \"Authorization\"  : \"abxxdsewrwsds3232ss\" }," +
 			"]";
-	
+
 		File currentParent;					// 记录当前的父文件夹
 		File[] currentFiles;				// 记录当前路径下的所有文件夹的文件数组
 		private int position = 1;           //记录当前文件位置
@@ -60,10 +60,10 @@ public class ListFileActivity extends ListActivity
 		//存入preference的操作对象
 		SettingPreference settingPreference;
 	    @Override
-	    public void onCreate( Bundle savedInstanceState ) 
+	    public void onCreate( Bundle savedInstanceState )
 	    {
 	        super.onCreate( savedInstanceState );
-			
+
 	        currentParent = root;
 			currentFiles = root.listFiles();
 			if (sort.hideFileNum(currentFiles) == currentFiles.length)
@@ -77,25 +77,29 @@ public class ListFileActivity extends ListActivity
 	        //启动service
 	        Intent intent = new Intent( this, MOfficeClientService.class);
 	        startService( intent );
-	        
+
 	        //实现将第三方包名写入文件，以便wps读取
 	        settingPreference = new SettingPreference(this);
 	        settingPreference.setSettingParam(Define.KEY, getPackageName());
+
+			// 打开文档流
+			Uri docUri = null;
+			openUri(docUri);
 	    }
-	    
+
 	    @Override
-	    public void onResume() 
+	    public void onResume()
 	    {
 	    	super.onResume();
 	    }
-	    
+
 	    @Override
-	    public void onPause() 
+	    public void onPause()
 	    {
 	    	super.onPause();
 	    }
-	    
-	    protected BroadcastReceiver broadcastReceiver = new BroadcastReceiver() 
+
+	    protected BroadcastReceiver broadcastReceiver = new BroadcastReceiver()
 	    {
 	    	@Override
 	    	public void onReceive( Context context, Intent intent )
@@ -103,20 +107,20 @@ public class ListFileActivity extends ListActivity
 	    		updateUI( intent );
 	    	}
 	    };
-	    
-	    private void updateUI( Intent intent ) 
+
+	    private void updateUI( Intent intent )
 	    {
 	    	Log.d( LOG_TAG, "Service ...... " );
 	    }
-	    
+
 		private long exitTime = 0;						//实现“再按一次退出程序”的时间检测
-		private long waitTime = 2000; 
+		private long waitTime = 2000;
 		@Override
-		public void onBackPressed() 
+		public void onBackPressed()
 		{
-			try 
+			try
 			{
-				if (!currentParent.getCanonicalPath().equals(rootPath)) 
+				if (!currentParent.getCanonicalPath().equals(rootPath))
 				{
 					currentParent = currentParent.getParentFile();
 					currentFiles = currentParent.listFiles();
@@ -126,33 +130,33 @@ public class ListFileActivity extends ListActivity
 				else
 				{
 					if((System.currentTimeMillis() - exitTime) > waitTime)
-					{  
-					    Toast.makeText(getApplicationContext(), "再按一次退出程序", 
-					    		Toast.LENGTH_SHORT).show();                                
-					    exitTime = System.currentTimeMillis();   
-					} 
-					else 
+					{
+					    Toast.makeText(getApplicationContext(), "再按一次退出程序",
+					    		Toast.LENGTH_SHORT).show();
+					    exitTime = System.currentTimeMillis();
+					}
+					else
 					{
 					    finish();
 					    System.exit(0);
 					}
 				}
-			} 
-			catch (IOException e) 
+			}
+			catch (IOException e)
 			{
 				e.printStackTrace();
 			}
 		}
-	
+
     @Override
-	protected void onListItemClick(ListView l, View v, int position, long id) 
+	protected void onListItemClick(ListView l, View v, int position, long id)
     {
 		super.onListItemClick(l, v, position, id);
-		if (currentFiles[position].isFile()) 
+		if (currentFiles[position].isFile())
 		{
-			
+
 			if (IsWPSFile(currentFiles[position]))
-			{	
+			{
 				//如果是wps能打开的文件，则打开
 				if (Define.WPS_OPEN_AIDL.equals(settingPreference.getSettingParam(Define.WPS_OPEN_MODE, "")))
 				{
@@ -179,27 +183,27 @@ public class ListFileActivity extends ListActivity
 					//以第三方方式打开
 					openFile(currentFiles[position].getAbsolutePath());
 				}
-				
+
 			}
 			else
 			{//不是wps文件则让用户选择
-				Intent intent = new Intent();  
-		        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);  
-		        intent.setAction(android.content.Intent.ACTION_VIEW);  
-		        String type = getMIMEType(currentFiles[position]);  
-		        intent.setDataAndType(Uri.fromFile(currentFiles[position]), type); 
-		        startActivity(intent);  
+				Intent intent = new Intent();
+		        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		        intent.setAction(android.content.Intent.ACTION_VIEW);
+		        String type = getMIMEType(currentFiles[position]);
+		        intent.setDataAndType(Uri.fromFile(currentFiles[position]), type);
+		        startActivity(intent);
 			}
 			return;
 		}
 		// 如果是目录，获取用户点击的文件夹 下的所有文件
 		File[] tem = currentFiles[position].listFiles();
-		if (tem == null || tem.length == 0) 
+		if (tem == null || tem.length == 0)
 		{
 			Toast.makeText(this,"当前路径下没有文件", Toast.LENGTH_LONG).show();
 			return;
-		} 
-		else 
+		}
+		else
 		{
 			if (sort.hideFileNum(tem) == tem.length)
 			{//如果目录下都是隐藏文件就返回
@@ -212,9 +216,9 @@ public class ListFileActivity extends ListActivity
 	        setListAdapter(new EfficientAdapter(this, currentFiles));
 		}
 	}
-    
+
     @Override
-	public boolean onCreateOptionsMenu(Menu menu) 
+	public boolean onCreateOptionsMenu(Menu menu)
     {
 		super.onCreateOptionsMenu(menu);
 		menu.add(0, 0, 0, "        设置常用参数");
@@ -226,9 +230,9 @@ public class ListFileActivity extends ListActivity
 	}
 
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) 
+	public boolean onOptionsItemSelected(MenuItem item)
 	{
-		switch (item.getItemId()) 
+		switch (item.getItemId())
 		{
 		case 0:
 			Intent intent = new Intent();
@@ -261,41 +265,42 @@ public class ListFileActivity extends ListActivity
 		return	true;
 
 	}
-    
-    
+
+
     /**
      * 识别文件的类型
      * @param f
      * @return
      */
     private String getMIMEType(File f)
-    {  
-        String end = f.getName().substring(f.getName().lastIndexOf(".") + 1,  
-                		f.getName().length()).toLowerCase();  
-        String type = "";  
-        if (end.equals("mp3") || end.equals("aac") || end.equals("aac") 
-        		|| end.equals("amr") || end.equals("mpeg") || end.equals("mp4"))  
-        {  
-          type = "audio";  
+    {
+        String end = f.getName().substring(f.getName().lastIndexOf(".") + 1,
+                		f.getName().length()).toLowerCase();
+        String type = "";
+        if (end.equals("mp3") || end.equals("aac") || end.equals("aac")
+        		|| end.equals("amr") || end.equals("mpeg") || end.equals("mp4"))
+        {
+          type = "audio";
         }
-        else if (end.equals("jpg") || end.equals("gif") || end.equals("png") || end.equals("jpeg"))  
-        {  
-          type = "image";  
-        } 
+        else if (end.equals("jpg") || end.equals("gif") || end.equals("png") || end.equals("jpeg"))
+        {
+          type = "image";
+        }
         else if (end.equals("doc") || end.equals("docx") || end.equals("pdf")
         		|| end.equals("txt"))
-        {  
-          type = "application/msword";  
+        {
+          type = "application/msword";
           return type;
-        }  
+        }
         else
         {
         	type = "*";
         }
-        type += "/*";  
-        return type;  
-    } 
-    
+        type += "/*";
+        return type;
+    }
+
+
   /**
    * 如果是wps文件，则用wps打开，并对其设置一下参数
    * @param path
@@ -303,6 +308,7 @@ public class ListFileActivity extends ListActivity
    */
   private boolean openFile(String path)
   {
+
 	  String wps_open_mode = settingPreference.getSettingParam(Define.WPS_OPEN_MODE, "null");
 	  if (wps_open_mode.equals(Define.WPS_OPEN_AIDL))
 	  {
@@ -314,42 +320,51 @@ public class ListFileActivity extends ListActivity
 		  Log.d("sort", "AIDL方式启动office");
 		  return true;
 	  }
-	  //获得上次打开的文件信息
-	  String 	closeFilePath 	= settingPreference.getSettingParam(Define.CLOSE_FILE, "null");  
-	  String 	packageName   	= settingPreference.getSettingParam(Define.THIRD_PACKAGE, getPackageName());
-	  float 	ViewProgress 	= settingPreference.getSettingParam(Define.VIEW_PROGRESS, (float)0.0);
-      float 	ViewScale 		= settingPreference.getSettingParam(Define.VIEW_SCALE, (float)1.0);
-      int 		ViewScrollX 	= settingPreference.getSettingParam(Define.VIEW_SCROLL_X, 0);
-      int 		ViewScrollY 	= settingPreference.getSettingParam(Define.VIEW_SCROLL_Y ,0);
-      String savePath           = settingPreference.getSettingParam(Define.SAVE_PATH, "");
+
+	  Bundle bundle = getBundle(path);
+	  Intent intent = getIntent(path);
+	  Uri uri = getUri(path);
+
+	  return openUri(uri, bundle, intent);
+  	}
+
+	private Bundle getBundle(String path) {
+
+		//获得上次打开的文件信息
+		String 	closeFilePath 	= settingPreference.getSettingParam(Define.CLOSE_FILE, "null");
+		String 	packageName   	= settingPreference.getSettingParam(Define.THIRD_PACKAGE, getPackageName());
+		float 	ViewProgress 	= settingPreference.getSettingParam(Define.VIEW_PROGRESS, (float)0.0);
+		float 	ViewScale 		= settingPreference.getSettingParam(Define.VIEW_SCALE, (float)1.0);
+		int 		ViewScrollX 	= settingPreference.getSettingParam(Define.VIEW_SCROLL_X, 0);
+		int 		ViewScrollY 	= settingPreference.getSettingParam(Define.VIEW_SCROLL_Y ,0);
+		String savePath           = settingPreference.getSettingParam(Define.SAVE_PATH, "");
 //	  String userName           = settingPreference.getSettingParam(Define.USER_NAME, "");
 //      String 	packageName 	= getPackageName();
-	    //获取用户设置的参数信息
-	    String	OpenMode		= settingPreference.getSettingParam(Define.OPEN_MODE, null);
-	    boolean   SendSaveBroad   = settingPreference.getSettingParam(Define.SEND_SAVE_BROAD, false);
-	    boolean   SendCloseBroad  = settingPreference.getSettingParam(Define.SEND_CLOSE_BROAD, false);
-	    boolean   IsIsClearBuffer = settingPreference.getSettingParam(Define.IS_CLEAR_BUFFER, false);
-	    boolean   IsClearTrace 	= settingPreference.getSettingParam(Define.IS_CLEAR_TRACE, false);
-	    boolean   IsClearFile 	= settingPreference.getSettingParam(Define.IS_CLEAR_FILE, false);
-	    boolean   IsViewScale     = settingPreference.getSettingParam(Define.IS_VIEW_SCALE ,false);
-	    boolean   AutoJump		= settingPreference.getSettingParam(Define.AUTO_JUMP, false);
-	    boolean   EnterReviseMode = settingPreference.getSettingParam(Define.ENTER_REVISE_MODE, false);
-	    boolean   CacheFileInvisible = settingPreference.getSettingParam(Define.CACHE_FILE_INVISIBLE, false);
+		//获取用户设置的参数信息
+		String	OpenMode		= settingPreference.getSettingParam(Define.OPEN_MODE, null);
+		boolean   SendSaveBroad   = settingPreference.getSettingParam(Define.SEND_SAVE_BROAD, false);
+		boolean   SendCloseBroad  = settingPreference.getSettingParam(Define.SEND_CLOSE_BROAD, false);
+		boolean   IsIsClearBuffer = settingPreference.getSettingParam(Define.IS_CLEAR_BUFFER, false);
+		boolean   IsClearTrace 	= settingPreference.getSettingParam(Define.IS_CLEAR_TRACE, false);
+		boolean   IsClearFile 	= settingPreference.getSettingParam(Define.IS_CLEAR_FILE, false);
+		boolean   IsViewScale     = settingPreference.getSettingParam(Define.IS_VIEW_SCALE ,false);
+		boolean   AutoJump		= settingPreference.getSettingParam(Define.AUTO_JUMP, false);
+		boolean   EnterReviseMode = settingPreference.getSettingParam(Define.ENTER_REVISE_MODE, false);
+		boolean   CacheFileInvisible = settingPreference.getSettingParam(Define.CACHE_FILE_INVISIBLE, false);
 
-		Intent intent = new Intent();
 		Bundle bundle = new Bundle();
 		bundle.putString(Define.OPEN_MODE, OpenMode);			     //打开模式
 		bundle.putBoolean(Define.SEND_SAVE_BROAD, SendSaveBroad);    //保存文件的广播
 		bundle.putBoolean(Define.SEND_CLOSE_BROAD, SendCloseBroad);	 //关闭文件的广播
 		bundle.putBoolean(Define.CLEAR_BUFFER, IsIsClearBuffer);	 //清除临时文件
-		bundle.putBoolean(Define.CLEAR_TRACE, IsClearTrace);		 //清除使用记录	
+		bundle.putBoolean(Define.CLEAR_TRACE, IsClearTrace);		 //清除使用记录
 		bundle.putBoolean(Define.CLEAR_FILE, IsClearFile);           //删除打开文件
 		bundle.putBoolean(Define.AUTO_JUMP, AutoJump);				//自动跳转，包括页数和xy坐标
 		bundle.putString(Define.THIRD_PACKAGE, packageName);
 		bundle.putString(Define.SAVE_PATH, savePath);               //保存路径
 		bundle.putBoolean(Define.CACHE_FILE_INVISIBLE, CacheFileInvisible);    //
 		bundle.putBoolean(Define.ENTER_REVISE_MODE, EnterReviseMode);    //
-//	    bundle.putString(Define.USER_NAME, userName);	
+//	    bundle.putString(Define.USER_NAME, userName);
 //		bundle.putString(Define.CHECK_PACKAGE_NAME, "cn.wps.moffice");
 //		bundle.putString(Define.JSON_DATA, JSON_DATA);              //特殊需求，直接跳过agent，连接client参数
 		if (path.equals(closeFilePath))						       //如果打开的文档时上次关闭的
@@ -363,7 +378,13 @@ public class ListFileActivity extends ListActivity
 				bundle.putInt(Define.VIEW_SCROLL_Y, ViewScrollY);			//y
 			}
 		}
-		
+
+		return  bundle;
+	}
+
+	private Intent getIntent(String path) {
+		Intent intent = new Intent();
+
 		intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 		intent.setAction(android.content.Intent.ACTION_VIEW);
 		if (checkPackage(Define.PACKAGENAME_ENT)){
@@ -382,48 +403,62 @@ public class ListFileActivity extends ListActivity
 		else
 		{
 			Toast.makeText(this,"文件打开失败，移动wps可能未安装", Toast.LENGTH_LONG).show();
-			return false;
+			return null;
 		}
-		
+
+		return intent;
+	}
+
+	private Uri getUri(String path) {
+
+
 		File file = new File(path);
 		if (file == null || !file.exists())
-		{			return false;
+		{			return null;
 		}
-		
+
 		Uri uri = Uri.fromFile(file);
+
+		return  uri;
+	}
+
+	private boolean openUri(Uri uri, Bundle bundle, Intent intent) {
+
 		intent.setData(uri);
 		intent.putExtras(bundle);
-		
-		try 
+
+		try
 		{
 			startActivity(intent);
 		}
-		catch (ActivityNotFoundException e) 
+		catch (ActivityNotFoundException e)
 		{
 			e.printStackTrace();
-			
+
 			return false;
 		}
 		Log.d("sort", "第三方打开wps");
 		return true;
-  	}
-   
-  	/**
+	}
+
+
+
+	/**
   	 * 判断是否是wps能打开的文件
   	 * @param file
   	 * @return
   	 */
   	private boolean IsWPSFile(File file)
   	{
-  		String end = file.getName().substring(file.getName().lastIndexOf(".") + 1,  
-        		file.getName().length()).toLowerCase();  
-  		if (end.equals("doc") || end.equals("docx") || end.equals("wps") 
-			|| end.equals("dot") || end.equals("wpt") 
-			|| end.equals("xls") || end.equals("xlsx") || end.equals("et") 
-			|| end.equals("ppt") || end.equals("pptx") || end.equals("dps") 
+  		String end = file.getName().substring(file.getName().lastIndexOf(".") + 1,
+        		file.getName().length()).toLowerCase();
+  		if (end.equals("doc") || end.equals("docx") || end.equals("wps")
+			|| end.equals("dot") || end.equals("wpt")
+			|| end.equals("xls") || end.equals("xlsx") || end.equals("et")
+			|| end.equals("ppt") || end.equals("pptx") || end.equals("dps")
 			|| end.equals("txt") || end.equals("pdf"))
   			return true;
-  		
+
   		return false;
   	}
 
@@ -432,22 +467,22 @@ public class ListFileActivity extends ListActivity
   	 * @param packageName
   	 * @return
   	 */
-  	public boolean checkPackage(String packageName) 
-  	{  
-	    if (packageName == null || "".equals(packageName))  
-	        return false;  
-	    try 
-	    {  
-	        getPackageManager().getApplicationInfo(packageName, PackageManager.GET_UNINSTALLED_PACKAGES);  
-	        return true;  
-	    } 
-	    catch (NameNotFoundException e) 
-	    {  
-	        return false;  
-	    }  
-	}  
+  	public boolean checkPackage(String packageName)
+  	{
+	    if (packageName == null || "".equals(packageName))
+	        return false;
+	    try
+	    {
+	        getPackageManager().getApplicationInfo(packageName, PackageManager.GET_UNINSTALLED_PACKAGES);
+	        return true;
+	    }
+	    catch (NameNotFoundException e)
+	    {
+	        return false;
+	    }
+	}
 	@Override
-	protected void onStop() 
+	protected void onStop()
 	{
 		super.onStop();
 		Log.v("stop", "stop");
@@ -455,48 +490,48 @@ public class ListFileActivity extends ListActivity
 	}
 
 	@Override
-	protected void onRestart() 
+	protected void onRestart()
 	{
 		super.onRestart();
 		Log.v("restart", "restart");
 
 	}
-	
-	
+
+
 	@Override
-	protected void onDestroy() 
+	protected void onDestroy()
 	{
 		//unregisterReceiver(writerBackKeyDownListerner);
 		//unregisterReceiver(writerHomeKeyDownListerner);
 		super.onDestroy();
 	}
-  	
-  	
-  	
+
+
+
 	private static final String ENT_WRITER_KEY_BACK_ACTION		= "com.kingsoft.writer.back.key.down";
 	IntentFilter filterBackKey = new IntentFilter(ENT_WRITER_KEY_BACK_ACTION);
-	private BroadcastReceiver writerBackKeyDownListerner = new BroadcastReceiver() 
+	private BroadcastReceiver writerBackKeyDownListerner = new BroadcastReceiver()
 	{
-		 
+
 	       @Override
-	       public void onReceive(Context context, Intent intent) 
+	       public void onReceive(Context context, Intent intent)
 	       {
 	    	   Log.d("MOfficeDemoActivity", "writerBackKeyDownListerner");
 	       }
-	 
+
 	  };
-	    
+
 	private static final String ENT_WRITER_KEY_HOME_ACTION		= "com.kingsoft.writer.home.key.down";
 	IntentFilter filterHomeKey = new IntentFilter(ENT_WRITER_KEY_HOME_ACTION);
 	private BroadcastReceiver writerHomeKeyDownListerner = new BroadcastReceiver()
 	{
-		 
+
 	       @Override
 	       public void onReceive(Context context, Intent intent)
 	       {
 	    	   Log.d("MOfficeDemoActivity", "writerHomeKeyDownListerner");
 	       }
- 
+
     };
 
 }
